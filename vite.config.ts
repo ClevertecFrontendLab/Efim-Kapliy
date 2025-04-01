66d7f0bc5440d63307451e2e641967a2c1c09f92
Efim-Kapliy <efim0424@mail.ru>
2025-04-01 18:21:07 +0300

    feat(config): add project opening on startup

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
+const NUMBER_PORT = 3000;
+
 export default defineConfig({
     plugins: [react()],
     server: {
         host: true,
-        port: 3000,
+        port: NUMBER_PORT,
+        open: true,
     },
     resolve: {
         alias: {
